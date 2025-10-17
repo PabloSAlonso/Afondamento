7390aed60a2b2146913725c8669a028907691b04
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-10-17 13:09:40 +0200

    changes

--- python/funciones.py
+++ python/funciones.py
@@ -1,28 +1,31 @@
 # Funciones
 import math
-# Sin parámetro ni return
+# Sin parámetro ni return 
 def nombre_funcion():
     print("Solo muestro en pantalla")
 def separador():
     print("\n-------------------\n")
+
 # Parámetros y valor devuelto
 def area_cilindro(radio, altura):
     area_base = math.pi * radio ** 2
     area_lateral = 2 * math.pi * radio * altura
     area_total = 2 * area_base + area_lateral
     return area_total
+
 # Devolver varios valores
 def calcular_todas_areas_cilindro(radio, altura):
     area_base = math.pi * radio ** 2
     area_lateral = 2 * math.pi * radio * altura
     area_total = 2 * area_base + area_lateral
     return area_total, area_lateral, area_base
+
 # Opcionalmente se pueden indicar tipos de parámetros y valor devuelto,
 # pero no se exige el cumplimiento. Solo es informativo.
-def suma(a:int, b:int)-> int:
+def suma(a:int, b:int)-> int: # Esa flecha es devolver (No lambda)
     return a+b
 
-nombre_funcion()
+nombre_funcion # Parentesis opcional
 separador()
 print(f"Area: {area_cilindro(3,4):.2f}")
 separador()
@@ -31,4 +34,8 @@ print(f"Área total: {total:.2f}")
 print(f"Área lateral: {lateral:.2f}")
 print(f"Área de una base: {base:.2f}")
 separador()
+
 print(suma("33","w")) # En teoría deberían ser int, pero el tipado no impone.
+a = print(suma("33","w")) # Esta burrada va bien
+print(a)
+print(type(a))
--- python/selectivas.py
+++ python/selectivas.py
@@ -13,13 +13,16 @@ print("Esto ya está fuera de la estructura por la identación.")
 respuesta = input("¿Deseas continuar?\n")
 # Las cadenas sí se pueden comparar con ==
 # Las operaciones lógicas son and, or y not
-if respuesta =="si" or respuesta == "Si" or respuesta == "SI":
-    print("Pues lo lamento porque por ahora voya a parar")
-    print("Fin del ejemplo")
-# El switch se realiza con varios if-elif-else
+if respuesta =="si" or respuesta == "Si" or respuesta == "SI": # El and es literal "and"
+    print("Pues lo lamento porque por ahora voy a a parar")
+print("Fin del ejemplo")
+
+# El switch se realiza con varios if-elif-else (el else será el default)
+
 # Existe la estructura match (similar a switch) desde Python 3.10, pero está poco extendida
+
 # Ternaria
-print("Positivo o 0" if numero >= 0 else "Negativo")
+print("Positivo o 0" if numero >= 0 else "Negativo") # Se ve natural pero limita ese acortamiento de la ternaria (parece codigo sin mas ahi metido)
 
 # ------------------------------------------------------------------------------
 
@@ -28,15 +31,18 @@ cont = 1
 acu = 0
 while cont < 11:
     acu = acu + cont
-    cont = cont + 1 # valido cont+=1 pero no ++
-    print("Acu = ", acu)
+    cont = cont + 1 # valido cont+=1 pero ++ no existe
+print("Acu = ", acu)
 
 # -------------------------------------------------------------------------------
 
 # Bucle For
+
 # El for es un foreach, siempre recorre una enumeración de elementos
 for i in range(10): # range(n) devuelve un tipo range: secuencia de números inmutable.
     print(i)
     print("-----")
 for i in range (5,30,2):
     print(i)
+
+# Al ser solo foreach se usa mas que nada para recorrer y mostrar pero no mutar/modificar followww
