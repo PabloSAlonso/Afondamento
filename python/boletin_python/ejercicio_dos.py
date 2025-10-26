aeb1fecbe03a3eedb241d67900af3f9c72557dff
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-10-26 18:32:13 +0100

    changes

---
+++ python/boletin_python/ejercicio_dos.py
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 2. Analizador de texto
+# a) Función contar_palabras: Recibe un texto y devuelve un diccionario con cada
+# palabra y su frecuencia (ignorando mayúsculas y signos de puntuación). Ejemplo:
+# # Texto de entrada:
+# texto = "El gato negro y el gato blanco juegan. El gato negro es
+# rápido."
+# # Diccionario que devolvería la función contar_palabras:
+# {
+#  'el': 3,
+#  'gato': 3,
+#  'negro': 2,
+#  'y': 1,
+#  'blanco': 1,
+#  'juegan': 1,
+#  'es': 1,
+#  'rápido': 1
+# }
+# b) Función estadisticas_texto: Recibe un texto y devuelve en una tupla:
+# • Número total de palabras.
+# • Longitud media de las palabras.
+# • Palabra más larga.
+# • Palabra más frecuente.
+# c) Programa principal: Pide el nombre de un archivo de texto al usuario y muestra
+# primero el diccionario que devuelve contar_palabras y luego las estadísticas que
+# devuelva estadisticas_texto. Presenta todos los datos de forma adecuada y bien
+# alineados. 
